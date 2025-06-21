0bdf2fa265f8e7134f0deaff9bc55c85dfdf5274
Kamron <134091599+kamronsafar@users.noreply.github.com>
2025-06-21 13:18:45 -0400

    Update run-64.sh

--- run-64.sh
+++ run-64.sh
@@ -1,3 +1,3 @@
-nasm -f elf64 boom-x86_64.asm -o boom-x86_64.o
-ld -o boom-x86_64  boom-x86_64.o
-./boom-x86_64
+nasm -f elf64 boom-x86_64.asm -o boomx8664.o
+ld -o boomx8664 boomx86_64.o
+./boomx8664
